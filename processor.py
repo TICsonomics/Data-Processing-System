7a1256baf340790c6d0cc6fc6120cd6403ddfd08
miguelzpresa <mikezpresa@gmail.com>
2023-06-10 20:38:11 -0600

    listo

--- processor.py
+++ processor.py
@@ -43,10 +43,13 @@ def graficador2(indi,temporalidad):
    global stock_df
    options={'rsi_14':"b" ,'macd':"g" ,'kdjk':"r" }
    color =options[indi]
-   figure = mpf.plot(hist, type='candle', figratio=(12, 8), title=f'{asset}________indicador:{indi}_________temporalidad:{temporalidad}', style='yahoo', 
-          mav=(12, 26), volume=True, 
-          addplot=[mpf.make_addplot(stock_df[indi], panel=1, color=color)])
-   return figure
+   
+   #style = mpf.make_mpf_style(base_mpf_style='yahoo', figcolor='w', facecolor='w', gridstyle=' ', gridcolor='w')
+   fig, ax = mpf.plot(hist, type='candle', figratio=(12, 8), title=f'{asset} Indicador: {indi} Temporalidad: {temporalidad}', style='yahoo',
+                   mav=(12, 26), volume=True,
+                   addplot=[mpf.make_addplot(stock_df[indi], panel=1, color=color)],
+                   returnfig=True)
+   return fig
    
 def graficador(df, columns, last_n, title):
     plot_df = df[columns].tail(last_n)
@@ -60,95 +63,103 @@ def main():
   global pro_df
   #start_date= input("start date")
   #close_price_date= input("close_price date")
-  indicator = int(input("1: RSI_14 \n 2:MACD \n 3:Indi_Stocástico \n 4:PPSR\n"))
-  tempo = int(input("1: 30 min \n 2:4 hrs \n 3:4days") )# 
-  #start_date = '2023-06-01'
-  #end_date = '2023-06-30'
-  db_params = {
-    'dbname': 'data_acquisition',
-    'user': 'acquisition',
-    'password': '0PZ9TVXV',
-    'host': 'localhost',
-    'port': '5432'
-  }
- 
-  db_conection = create_engine('postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(dbname)s' % db_params) 
-  #DATABASE_URI = 'postgres+psycopg2://username:password@localhost:5432/database_name'
-  #engine = create_engine(DATABASE_URI)
-  # Consulta SQL para obtener los datos filtrados
-
-  """---------------+--------
- bitcoin       | BTC
- ethereum      | ETH
- ripple        | XRP
- matic-network | MATIC
- polkadot      | DOT"""
-
   choose = int(input("choose assets ticket:\n 1:bitcoin \n2:ethereum \n3:ripple \n 4:matic_network\n 5:polkadot"))
   coins={1:"bitcoin" ,2:"ethereum" ,3:"ripple" ,4:"matic_network",5:"polkadot"}
   asset =coins[choose]
-
-  temporalidad =""
-  if tempo ==1:
-    #asset = "bitcoin"  # Asset seleccionado
-    #query = f"SELECT * FROM half_hours WHERE coin_id = '{asset}' ORDER BY date_price DESC LIMIT 90"# WHERE date_price >= '{start_date}'"# AND date_price <= '{close_price_date}'"
-    query = f"SELECT date_price  , open_price , high_price , low_price ,close_price , volume   FROM half_hour;" #ORDER BY date_price DESC LIMIT 90"
-    temporalidad="30 minutos"
-  elif tempo ==2:
-    temporalidad="4 horas"
-    query = f"SELECT date_price  , open_price , high_price , low_price ,close_price , volume FROM four_hours;" #ORDER BY date_price DESC LIMIT 90"
-    #asset = "ethereum"  
-    #query = f"SELECT * FROM four_hours WHERE coin_id = '{asset}' ORDER BY date_price DESC LIMIT 90"# WHERE date_price >= '{start_date}'"# AND date_price <= '{close_price_date}'"
-  elif tempo == 3:
-    temporalidad="4 días"
-    query = f"SELECT date_price  , open_price , high_price , low_price ,close_price , volume FROM four_days;" #ORDER BY date_price DESC LIMIT 90"
-    #asset = "ripple"  
-    #query = f"SELECT * FROM four_days WHERE coin_id = '{asset}' ORDER BY date_price DESC LIMIT 90"# WHERE date_price >= '{start_date}'"# AND date_price <= '{close_price_date}'"
-
-
-  hist = pd.read_sql_query(query,db_conection)
-  print(type(hist))
-  hist.to_csv('h.csv', index=False)
-  hist.drop(hist.columns[0], axis=1, inplace=True)
-  hist.rename(columns={'close_price': 'close','open_price':'open','high_price':'high','low_price':'low'}, inplace=True)
-
-
-  stock_df = stockstats.StockDataFrame.retype(hist)#,convert={'close': 'close_price'})
-  #print(stock_df.columns)
-  hist.index = pd.to_datetime(hist.index)
-  if indicator ==1:
-    #pro_df = MA(hist, 7)
-    #stock_df['close_price'] 
-    pro_df =stock_df['rsi_14'] 
-    indi = 'rsi_14'
-    fig = graficador2(indi,temporalidad)
-  elif indicator ==2:
+  for indicador in range(4):
+     indicator= indicador + 1
+     for tiempo in range(3):
+      #indicator = int(input("1: RSI_14 \n 2:MACD \n 3:Indi_Stocástico \n 4:PPSR\n"))
+      #tempo = int(input("1: 30 min \n 2:4 hrs \n 3:4days") )# 
+      tempo = tiempo +1
+      #start_date = '2023-06-01'
+      #end_date = '2023-06-30'
+      db_params = {
+        'dbname': 'data_acquisition',
+        'user': 'acquisition',
+        'password': '0PZ9TVXV',
+        'host': 'localhost',
+        'port': '5432'
+      }
     
-    pro_df =stock_df['macd'] 
-    indi = 'macd'
-    fig = graficador2(indi,temporalidad)
-  elif indicator ==3:
-    stock_df['kdjk']
-    indi="indicador estocástico"
-    fig = graficador2(indi,temporalidad)
+      db_conection = create_engine('postgresql://%(user)s:%(password)s@%(host)s:%(port)s/%(dbname)s' % db_params) 
+      #DATABASE_URI = 'postgres+psycopg2://username:password@localhost:5432/database_name'
+      #engine = create_engine(DATABASE_URI)
+      # Consulta SQL para obtener los datos filtrados
+
+      """---------------+--------
+    bitcoin       | BTC
+    ethereum      | ETH
+    ripple        | XRP
+    matic-network | MATIC
+    polkadot      | DOT"""
+
+      
+
+      temporalidad =""
+      if tempo ==1:
+        #asset = "bitcoin"  # Asset seleccionado
+        #query = f"SELECT * FROM half_hours WHERE coin_id = '{asset}' ORDER BY date_price DESC LIMIT 90"# WHERE date_price >= '{start_date}'"# AND date_price <= '{close_price_date}'"
+        query = f"SELECT date_price  , open_price , high_price , low_price ,close_price , volume   FROM half_hour;" #ORDER BY date_price DESC LIMIT 90"
+        temporalidad="30 minutos"
+      elif tempo ==2:
+        temporalidad="4 horas"
+        query = f"SELECT date_price  , open_price , high_price , low_price ,close_price , volume FROM four_hours;" #ORDER BY date_price DESC LIMIT 90"
+        #asset = "ethereum"  
+        #query = f"SELECT * FROM four_hours WHERE coin_id = '{asset}' ORDER BY date_price DESC LIMIT 90"# WHERE date_price >= '{start_date}'"# AND date_price <= '{close_price_date}'"
+      elif tempo == 3:
+        temporalidad="4 días"
+        query = f"SELECT date_price  , open_price , high_price , low_price ,close_price , volume FROM four_days;" #ORDER BY date_price DESC LIMIT 90"
+        #asset = "ripple"  
+        #query = f"SELECT * FROM four_days WHERE coin_id = '{asset}' ORDER BY date_price DESC LIMIT 90"# WHERE date_price >= '{start_date}'"# AND date_price <= '{close_price_date}'"
 
 
-  elif indicator ==4:
-    #print("PIVOT POINTS, SUPPORTS AND RESISTANCES")
-    pro_df = PPSR(hist)
-    fig =graficador(pro_df, ['close_price', 'PP', 'R1', 'S1', 'R2', 'S2', 'R3', 'S3'], 90, 'PPSR plot')
+      hist = pd.read_sql_query(query,db_conection)
+      print(type(hist))
+      hist.to_csv('h.csv', index=False)
+      hist.drop(hist.columns[0], axis=1, inplace=True)
+      hist.rename(columns={'close_price': 'close','open_price':'open','high_price':'high','low_price':'low'}, inplace=True)
+
+
+      stock_df = stockstats.StockDataFrame.retype(hist)#,convert={'close': 'close_price'})
+      #print(stock_df.columns)
+      hist.index = pd.to_datetime(hist.index)
+      if indicator ==1:
+        #pro_df = MA(hist, 7)
+        #stock_df['close_price'] 
+        pro_df =stock_df['rsi_14'] 
+        indi = 'rsi_14'
+        fig = graficador2(indi,temporalidad)
+      elif indicator ==2:
+        
+        pro_df =stock_df['macd'] 
+        indi = 'macd'
+        fig = graficador2(indi,temporalidad)
+      elif indicator ==3:
+        stock_df['kdjk']
+        indi='kdjk'
+        fig = graficador2(indi,temporalidad)
+
+
+      elif indicator ==4:
+        #print("PIVOT POINTS, SUPPORTS AND RESISTANCES")
+        pro_df = PPSR(hist)
+        fig =graficador(pro_df, ['close_price', 'PP', 'R1', 'S1', 'R2', 'S2', 'R3', 'S3'], 90, 'PPSR plot')
 
 
 
-  # Borrar el contenido de la tabla 'procesados' si existe
-  #db_conection.execute(f"DROP TABLE IF EXISTS {processed_data}")
+      
+      #db_conection.execute(f"DROP TABLE IF EXISTS {processed_data}")
 
-  #pro_df.to_sql(processed_data, engine, index=False)
+      #pro_df.to_sql(processed_data, engine, index=False)
 
 
-  print("Procesamiento y almacenamiento de datos completados.")
-  fig.savefig('nombre_archivo.png')
-  pro_df.to_csv('nombre_archivo.csv', index=False)
+      print("Procesamiento y almacenamiento de datos completados.")
+      try:
+        fig.savefig('images_output/{}-{}-{}.png'.format(asset, indi, temporalidad))
+      except AttributeError:
+        plt.savefig('images_output/{}-{}-{}.png'.format(asset, indi, temporalidad))
+      pro_df.to_csv('pdfs_output/{}-{}-{}.csv'.format(asset, indi, temporalidad), index=False)
 if __name__ == "__main__":
-  fig =main()
-  plt.show()
+  main()
+  
